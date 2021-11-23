45fd34dd607db6d67fe184ca69e27870a9547a2b
matt-chap <eatsalldaylol@hotmail.com>
2021-11-22 22:43:49 -0500

    Print formatting

---
+++ GoStandardLib/formatting/printf/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+
+	//Decimal
+	fmt.Printf("%d\n", x)
+
+	//hexadecimal
+	fmt.Printf("%x\n", x)
+
+	//bool
+	fmt.Printf("%t\n", x > 10)
+
+	fmt.Printf("%f\n", 10.23)
+	fmt.Printf("%e\n", 10.23)
+
+	//explicit arguments
+	fmt.Printf("%[2]d %[1]d\n", 22, 33)
+
+	//print struct with properties
+	c := circle{
+		radius: 10,
+		border: 2,
+	}
+	fmt.Printf("%+v\n", c)
+}
+
+type circle struct {
+	radius int
+	border int
+}
