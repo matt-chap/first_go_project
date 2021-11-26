eca4d02ccef6f572088971b70594ca0dc69c84a9
matt-chap <eatsalldaylol@hotmail.com>
2021-11-25 23:22:18 -0500

    More strinf testing

--- GoStandardLib/stringOperations/main.go
+++ GoStandardLib/stringOperations/main.go
@@ -29,4 +29,25 @@ func main() {
 
 	x3 := strings.Title(x)
 	fmt.Println(x3)
+
+	//string searches
+	fmt.Println("strings.Contains(x, \"jump\")", strings.Contains(x, "jump"))
+	fmt.Println("strings.ContainsAny(x, \"abcd\")", strings.ContainsAny(x, "abcd"))
+
+	//Index
+	fmt.Println("strings.Index(x, \"fox\")", strings.Index(x, "fox"))
+	fmt.Println("strings.Index(x, \"cat\")", strings.Index(x, "cat"))
+	fmt.Println("strings.IndexAny(x, \"abcd\")", strings.IndexAny(x, "abcd")) // c is in the 7th spot
+
+	//Prefix or Suffix
+	fmt.Println("strings.HasPrefix(x, \"The\")", strings.HasPrefix(x, "The"))
+	fmt.Println("strings.HasPrefix(x, \"the\")", strings.HasPrefix(x, "the"))
+
+	fmt.Println("strings.HasSuffix(x, \"Dog\")", strings.HasSuffix(x, "Dog"))
+	fmt.Println("strings.HasSuffix(x, \"dog\")", strings.HasSuffix(x, "dog"))
+
+	//Count
+	fmt.Println("strings.Count(x, \"the\")", strings.Count(x, "the"))
+	fmt.Println("strings.Count(x, \"he\")", strings.Count(x, "he"))
+
 }
